139e881ff852a33b0e62f62b8d411fa42d044e56
Guido Laghi <guido321@gmail.com>
2016-04-22 00:39:48 -0300

    first rework for menu

--- Android/app/src/main/java/ar/fi/uba/trackerman/activities/MainActivity.java
+++ Android/app/src/main/java/ar/fi/uba/trackerman/activities/MainActivity.java
@@ -2,24 +2,27 @@ package ar.fi.uba.trackerman.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import ar.fi.uba.trackerman.domains.Order;
 import ar.fi.uba.trackerman.tasks.GetDraftOrdersTask;
-import ar.fi.uba.trackerman.tasks.GetOrderTask;
 import ar.fi.uba.trackerman.utils.AppSettings;
 import fi.uba.ar.soldme.R;
 
-public class MainActivity extends AppCompatActivity implements GetDraftOrdersTask.DraftOrdersValidation {
+public class MainActivity extends AppCompatActivity implements
+        GetDraftOrdersTask.DraftOrdersValidation,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private Order order;
 
@@ -28,6 +31,15 @@ public class MainActivity extends AppCompatActivity implements GetDraftOrdersTas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
@@ -49,6 +61,10 @@ public class MainActivity extends AppCompatActivity implements GetDraftOrdersTas
                 }
             }
         });
+        */
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         new GetDraftOrdersTask(this).execute(String.valueOf(AppSettings.getVendorId()));
 
@@ -88,5 +104,41 @@ public class MainActivity extends AppCompatActivity implements GetDraftOrdersTas
         Intent intent = new Intent(this, OrderActivity.class);
         intent.putExtra(Intent.EXTRA_UID,14l);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            openMyClientsActivity(null);
+        } else if (id == R.id.nav_gallery) {
+            openProductsActivity(null);
+        } else if (id == R.id.nav_slideshow) {
+            Toast.makeText(getApplicationContext(), "sin implementar", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_manage) {
+            Toast.makeText(getApplicationContext(), "sin implementar", Toast.LENGTH_LONG).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
