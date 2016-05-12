c5e7799fd22303048a975dcf300f5466df8db127
Pablo Lucadei <pablonazareno@gmail.com>
2016-05-12 00:44:57 -0300

    Implement Push Notifications

---
+++ Android/app/src/main/java/fi/uba/ar/soldme/services/MyGcmListenerService.java
@@ -0,0 +1,171 @@
+package fi.uba.ar.soldme.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ar.fi.uba.trackerman.activities.ClientActivity;
+import ar.fi.uba.trackerman.activities.MainActivity;
+import ar.fi.uba.trackerman.activities.ProductActivity;
+import fi.uba.ar.soldme.R;
+
+public class MyGcmListenerService extends GcmListenerService {
+
+    private static final String TAG = "MyGcmListenerService";
+    private static final String NEW_CLIENT = "NEW_CLIENT";
+    private static final String CLIENT_UPDATED = "CLIENT_UPDATED";
+    private static final String PRODUCT_STOCKED = "PRODUCT_STOCKED";
+
+
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String identifier = data.getString("identifier");
+        String type= data.getString("type");
+        String message= data.getString("message");
+        String picture= data.getString("picture");
+        Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Tipo: "+ type + " , Id: " +identifier );
+
+        if (from.startsWith("/topics/")) {
+            showNewPromotionNotification(message);
+        } else {
+            if(NEW_CLIENT.equals(type)){
+                showNewClientNotification(message,identifier,picture);
+            }else if(CLIENT_UPDATED.equals(type)){
+                showClientUpdatedNotification(message,identifier,picture);
+            }else if(PRODUCT_STOCKED.equals(type)){
+                showProductStockedNotification(message,identifier,picture);
+            }
+        }
+    }
+
+    private void showNewPromotionNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(getString(R.string.new_promotion))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    private void showNewClientNotification(String message, String clientId, String picture) {
+        Intent intent = new Intent(this, ClientActivity.class);
+        intent.putExtra(Intent.EXTRA_UID,clientId);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(getString(R.string.new_client))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        if(picture!=null && !picture.isEmpty()){
+            notificationBuilder.setLargeIcon(this.getBitmapFromURL(picture));
+        }
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    private void showClientUpdatedNotification(String message, String clientId, String picture) {
+        Intent intent = new Intent(this, ClientActivity.class);
+        intent.putExtra(Intent.EXTRA_UID,clientId);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(getString(R.string.client_updated))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        if(picture!=null && !picture.isEmpty()){
+            notificationBuilder.setLargeIcon(this.getBitmapFromURL(picture));
+        }
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    private void showProductStockedNotification(String message, String productId, String picture) {
+        Intent intent = new Intent(this, ProductActivity.class);
+        intent.putExtra(Intent.EXTRA_UID,productId);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(getString(R.string.product_stocked))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        if(picture!=null && !picture.isEmpty()){
+            notificationBuilder.setLargeIcon(this.getBitmapFromURL(picture));
+        }
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    public Bitmap getBitmapFromURL(String strURL) {
+        try {
+            URL url = new URL(strURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
